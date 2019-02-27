2e5a0610ae0c519ed3c2411096114fc4267f31fd
GreenWhite <greenwhitedev@gmail.com>
2019-02-27 08:27:26 +0100

    starting work on materials upload

---
+++ app/Student.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    //
+    protected $appends=['full_name'];
+    /**
+     * drg >> Set the full name for requests
+     * @param $input
+     */
+    public function getFullNameAttribute(){
+        return "$this->first_name $this->last_name";
+    }
+}
---
+++ app/TestsResultsAnswersReview.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TestsResultsAnswersReview extends Model
+{
+    //
+    protected $fillable
+        = [
+            'tests_results_answer_id',
+            'review',
+            'score'
+        ];
+
+}
