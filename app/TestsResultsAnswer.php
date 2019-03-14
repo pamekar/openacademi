a3f51f0dc4609c5a3f882f8ed6117e00814bc337
GreenWhite <greenwhitedev@gmail.com>
2019-03-14 18:30:33 +0100

    Added answer type and answer_text to fillable

--- app/TestsResultsAnswer.php
+++ app/TestsResultsAnswer.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class TestsResultsAnswer extends Model
 {
 
-    protected $fillable = ['tests_result_id', 'question_id', 'option_id', 'correct'];
+    protected $fillable = ['tests_result_id', 'question_id', 'option_id', 'correct','answer_text','answer_type'];
 
     public function question(){
         return $this->belongsTo('App\Question');
