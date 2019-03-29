7556a087749fbfd1fea6c6faac3bb6e23c0db3e3
GreenWhite <greenwhitedev@gmail.com>
2019-03-29 20:07:14 +0100

    scoped

---
+++ app/Instructor.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Instructor extends Model
+{
+    //
+
+    protected $appends=[
+        'full_name'
+    ];
+    public function getFullNameAttribute(){
+        return "$this->first_name $this->last_name";
+    }
+}
