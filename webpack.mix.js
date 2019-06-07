5c3c19ab4a53bacf50b6e065ef0170bee990e6ec
GreenWhite <greenwhitedev@gmail.com>
2019-06-07 08:54:53 +0100

    Setup Version Assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,11 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/home/app.js', 'public/js');
-mix.js('resources/js/student/app.js', 'public/student/js')
-    .sass('resources/sass/student/app.scss', 'public/student/css');
-mix.js('resources/js/teacher/app.js', 'public/teacher/js')
-    .sass('resources/sass/teacher/app.scss', 'public/teacher/css');
-
-if (mix.inProduction()) {
+mix.js('resources/js/student/app.js', 'public/student/js');
+mix.js('resources/js/teacher/app.js', 'public/teacher/js');
+if (mix.Mix.inProduction) {
     mix.version();
 }
+
