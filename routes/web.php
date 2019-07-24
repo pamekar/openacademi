248fdc72687026dd5bfcd2ff5c59a0d37a00f4cc
GreenWhite <greenwhitedev@gmail.com>
2019-07-24 22:19:31 +0100

    Download Routes for MediaController

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use TCG\Voyager\Facades\Voyager;
-use \Illuminate\Support\Facades\Auth;
 
 
 Route::get('courses/search',
@@ -38,6 +38,7 @@ Route::patch('change_password', 'Auth\ChangePasswordController@changePassword')
 
 Route::get('faq', 'HomeController@faq')->name('faq');
 
+// logged in user dashboard
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/user/{all?}', function () {
         if (Auth::user()->isStudent()) {
@@ -62,17 +63,23 @@ Route::get('/login/callback/{provider}', 'Auth\SocialController@callback');
 
 Route::group(['middleware' => 'checkLoggedIn'], function () {
     Route::group([
+        'namespace'  => 'Student',
+        'middleware' => 'auth'
+    ],
+        function () {
+            Route::get('lesson/{id}/{slug}/download',
+                'LessonsController@download');
+        });
+});
+
+Route::group(['middleware' => 'checkLoggedIn'], function () {
+    Route::group([
         'prefix'     => 'instructor',
         'namespace'  => 'Instructor',
         'middleware' => 'auth'
     ],
         function () {
-            Route::post('/spatie/media/upload',
-                'SpatieMediaController@create')
-                ->name('media.upload');
-            Route::post('/spatie/media/remove',
-                'SpatieMediaController@destroy')
-                ->name('media.remove');
+            Route::resource('media', 'MediaController');
         });
 });
 
