fda182ba60756601e5e6f389a0da13e86c40b9dc
GreenWhite <greenwhitedev@gmail.com>
2019-07-24 22:26:47 +0100

    Fixed bug with uploader config
    The supporting library was modified

--- resources/js/teacher/app.js
+++ resources/js/teacher/app.js
@@ -3,12 +3,10 @@ import VueRouter from 'vue-router';
 import BootstrapVue from 'bootstrap-vue';
 import VueHeadful from 'vue-headful';
 import './uiux/css/nprogress.css';
-import './uiux/css/nprogress.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue';
 import StarRating from 'vue-star-rating';
 import Breadcrumb from './components/BreadcrumbComponent.vue';
-import axios from 'axios';
 import router from './routes.js';
 import functions from './functions.js';
 import store from './store/index.js';
@@ -22,7 +20,7 @@ var csrf_token = document.querySelector('meta[name=csrf-token]').getAttribute('c
 
 // v-uploader plugin global config
 const uploaderConfig = {
-    uploadCustomHeaders:  {
+    uploadHeaders: {
             Authorization:  'Bearer ' + functions.readCookie('jwt_token')
     },
     // file uploader service url
@@ -30,7 +28,7 @@ const uploaderConfig = {
     // file delete service url
     deleteFileUrl:       '/api/instructor/spatie/media/remove',
     // set the way to show upload message(upload fail message)
-    showMessage:         (vue, message) => {
+    showMessage:   (vue, message) => {
         //using v-dialogs to show message
         vue.$vDialog.alert(message, null, {messageType: 'error'});
     }
